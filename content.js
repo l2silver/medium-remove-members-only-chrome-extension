b03a01d69deb092e8ffbdf392828c230b24460e3
Leigh Silverstein <lsilversteinto@gmail.com>
2017-11-03 16:36:00 -0400

    initial commit

---
+++ content.js
@@ -0,0 +1,11 @@
+function removeMediumTags(){
+  setTimeout(()=>{
+    $("span.svgIcon.svgIcon--lockStory").closest('div.heroGrid-largeCard').remove();
+    $("span.svgIcon.svgIcon--lockStory").closest('div.heroGrid-smallCard').remove();
+    $("span.svgIcon.svgIcon--lockStory").closest('div.heroGrid-compact').remove();
+    $("span.svgIcon.svgIcon--lockStory").closest('div.u-borderBox.js-sectionItem').remove();
+    removeMediumTags();
+  }, 500);
+}
+removeMediumTags();
+
