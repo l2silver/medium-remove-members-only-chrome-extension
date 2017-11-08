3c7f709b01bf59177eae43baa438c4772a4ba543
Leigh Silverstein <lsilversteinto@gmail.com>
2017-11-08 10:26:11 -0500

    update content to include mediumCard

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 function removeMediumTags(){
   setTimeout(()=>{
     $("span.svgIcon.svgIcon--lockStory").closest('div.heroGrid-largeCard').remove();
+    $("span.svgIcon.svgIcon--lockStory").closest('div.heroGrid-mediumCard').remove();
     $("span.svgIcon.svgIcon--lockStory").closest('div.heroGrid-smallCard').remove();
     $("span.svgIcon.svgIcon--lockStory").closest('div.heroGrid-compact').remove();
     $("span.svgIcon.svgIcon--lockStory").closest('div.u-borderBox.js-sectionItem').remove();
