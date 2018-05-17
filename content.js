0b30e7ca1a79f4dc969e0a24af2bcac51e86786b
l2silver <l2silver@ryerson.ca>
2018-05-16 21:27:03 -0400

    Update content.js

--- content.js
+++ content.js
@@ -4,10 +4,10 @@ function removeMediumTags(){
     $(icon).closest('div.extremeHero-largeCard').remove();
     $(icon).closest('div.extremeHero-mediumCard').remove();
     $(icon).closest('div.extremeHero-smallCard').remove();
+    $(icon).closest('div.streamItem').remove();
     $(icon).closest('div.extremeHero-compact').remove();
     $(icon).closest('div.u-borderBox.js-sectionItem').remove();
     removeMediumTags();
   }, 500);
 }
 removeMediumTags();
-
